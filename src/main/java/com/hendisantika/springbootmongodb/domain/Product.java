f039025b6be5558619351adb04edb62372bf645e
Hendi Santika <hendisantika@gmail.com>
2018-06-03 15:35:01 +0700

    #3 - Add Product Domain

---
+++ src/main/java/com/hendisantika/springbootmongodb/domain/Product.java
@@ -0,0 +1,74 @@
+package com.hendisantika.springbootmongodb.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/06/18
+ * Time: 15.34
+ * To change this template use File | Settings | File Templates.
+ */
+@Document(collection = "products")
+public class Product {
+    @Id
+    String id;
+    String prodName;
+    String prodDesc;
+    Double prodPrice;
+    String prodImage;
+
+    public Product() {
+    }
+
+    public Product(String prodName, String prodDesc, Double prodPrice, String prodImage) {
+        this.prodName = prodName;
+        this.prodDesc = prodDesc;
+        this.prodPrice = prodPrice;
+        this.prodImage = prodImage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public String getProdDesc() {
+        return prodDesc;
+    }
+
+    public void setProdDesc(String prodDesc) {
+        this.prodDesc = prodDesc;
+    }
+
+    public Double getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(Double prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+
+    public String getProdImage() {
+        return prodImage;
+    }
+
+    public void setProdImage(String prodImage) {
+        this.prodImage = prodImage;
+    }
+}
